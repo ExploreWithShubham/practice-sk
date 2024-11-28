9390cbe184649189607d17730a02a0f0d30913d7
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-11-28 11:07:51 +0530

    An Arrow Function and Normal Function

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 /*
-Write a Progrom to pring 1 to 100
+Function of JavaScript
 */
 
-for (let i = 1; i <= 100; i++)
-{
-  console.log(i);
+const addOne = (num) => {
+  return num + 1;
 }
+
+// function addOne(num) {
+//   return num + 1;
+// }
+
+const arr = [4, 5, 6, 7, 8];
+
+for (let i = 0; i < arr.length; i++)
+{
+  arr[i] = addOne(arr[i]);
+}
+
+console.log(arr);
