00e05b6f0f62da2de43cb14915bf1ca082532994
ExploreWithShubham <shubhamkum2910@gmail.com>
2024-11-29 21:04:45 +0530

    solved

--- server.js
+++ server.js
@@ -5,13 +5,27 @@ With Arrow a function
 
 Here is some test with target
 */
-test1 = []
-test2 = [1,2,3,4,5]
-test3 = [9,8,7,6,5]
-test4 = [2]
+const linearSearch = (arr,target) => {
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] == target) {
+      return i;
+    }
+  }
+  return -1;
+} 
 
-target1 = 2
-target2 = 4
-target3 = 5
-target4 = 2
 
+
+//test1 = []
+ test2 = [1,2,3,4,5]
+// test3 = [9,8,7,6,5]
+// test4 = [2]
+
+//target1 = 2
+ target2 = 4
+// target3 = 5
+// target4 = 2
+
+const result = linearSearch(test2,target2);
+
+console.log("Element found at index is %d ", result);
