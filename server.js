1f5e9f0676f2f1c15233683593b4db7e4cb3a4b5
root <Ammanrizwan00007@gmail.com>
2024-11-29 15:09:36 +0000

    new question

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 /*
-Function of JavaScript
+You have array of number find the index of the given target if target is not in the list return -1
+
+With Arrow a function 
+
+Here is some test with target
 */
+test1 = []
+test2 = [1,2,3,4,5]
+test3 = [9,8,7,6,5]
+test4 = [2]
 
-const addOne = (num) => {
-  return num + 1;
-}
+target1 = 2
+target2 = 4
+target3 = 5
+target4 = 2
 
-// function addOne(num) {
-//   return num + 1;
-// }
-
-const arr = [4, 5, 6, 7, 8];
-
-for (let i = 0; i < arr.length; i++)
-{
-  arr[i] = addOne(arr[i]);
-}
-
-console.log(arr);
