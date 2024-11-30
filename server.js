0754481b34e917dffda61bb1f7af30625c251ef8
ExploreWithShubham <shubhamkum2910@gmail.com>
2024-11-30 14:01:08 +0530

    solved

--- server.js
+++ server.js
@@ -16,19 +16,99 @@ let product =  [
     // image key missing
     // old_price missing
     "name" : "Carrot", 
+    image: "", 
     "discount" : 53,
     "quantity" : 1 ,
-    "price" : 51.5 // rename price as new_price
+    "new_price" : 51.5, // rename price as new_price
 
   },
 
   {
+    
     "name" : "Ladies finger",
+    image: "", 
     "discount" : 51,
     "quantity" : 1 ,
-    "price" : 36
+    "price" : 36,
+
+  },
+
+  {
+    
+    "name" : "Tomato",
+    image: "", 
+    "discount" : 39,
+    "quantity" : 2 * 1 ,
+    "price" : 92,
+
+  },
+  {
+    
+    "name" : "Cucumber",
+    image: "", 
+    "discount" : 51,
+    "quantity" : 1 ,
+    "price" : 24,
+
+  },
+  {
+    
+    "name" : "Corainder",
+    image: "", 
+    "discount" : 20,
+    "quantity" : 100 ,
+    "price" : 12.06,
+
+  },
+  {
+    
+    "name" : "Cauliflower",
+    image: "", 
+    "discount" : 27,
+    "quantity" : 1 ,
+    "price" : 26,
+
+  },
+
+  {
+    
+    "name" : "Capcicum",
+    image: "", 
+    "discount" : 50,
+    "quantity" : 1 ,
+    "price" : 38,
+
+  },
+
+  {
+    
+    "name" : "Banana",
+    image: "", 
+    "discount" : 23,
+    "quantity" : 1 ,
+    "price" : 70,
+
+  },
+
+  {
+    
+    "name" : "Bottle Gaurd",
+    image: "", 
+    "discount" : 37,
+    "quantity" : 1 ,
+    "price" : 25,
+
+  },
+
+  {
+    
+    "name" : "Dasheri Mango",
+    image: "", 
+    "discount" : 20,
+    "quantity" : 1 ,
+    "price" : 103.02,
 
   },
 ]
 
-console.log(product);
+console.log(product.name("Bottle Gaurd"));
