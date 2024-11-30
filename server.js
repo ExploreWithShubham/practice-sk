276b1b4c22ec70d6068cb109cc0e4d19f77b5c52
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-11-30 12:24:03 +0530

    some change

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ Go to the README.md File
 
 let product =  [
   {
-    "name" : "Potato",
-    "discount" : 24,
-    "quantity" : 5 ,
-    "price" : 49.4
-
+    name : "Potato",
+    discount : 24,
+    quantity : 5,
+    price : 49.4
   },
 ]
 
